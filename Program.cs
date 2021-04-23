e6bda72c33dd179d2dbc2e3761fd57956b19d700
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-23 09:57:18 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> selector = str => str.ToUpper();
+
+            // create an array of strings
+            string[] words = { "orange", "apple", "Article", "elephant" };
+            //query the array and select strings according  to the selector method
+            string[] bigLetterWords = words.Select(selector).ToArray();
+            //output the results to  the console
+
+            foreach (var word in bigLetterWords)
+            {
+                Console.WriteLine(word);
+            }
+        }        
+    }
+}
